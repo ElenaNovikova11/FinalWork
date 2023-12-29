e471e60f373db203e8de67d8a66cc7a98c939f20
elena <princessa-len4ik@mail.ru>
2023-12-29 15:51:00 +0500

    выводим результат на экран

--- Program.cs
+++ Program.cs
@@ -24,3 +24,16 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+Console.Clear();
+Console.Write("Введите число элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+Console.Write("Введите числа, которые будут составлять массив: ");
+InputArray(array);
+        Console.Write("Исходный массив: ");
+        Console.WriteLine(string.Join(", ", array));
+     
+Console.WriteLine("Значения, у которых символов больше или равно 3:");
+string[] array2 = new string[array.Length];
+SecondArray(array, array2);
+PrintArray(array2);
