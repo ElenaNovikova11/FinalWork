ac51ae45dac7d7154bbe1fff85ea7a70d8372dd4
elena <princessa-len4ik@mail.ru>
2023-12-29 15:50:25 +0500

    делаем выборку

--- Program.cs
+++ Program.cs
@@ -3,3 +3,24 @@
 for (int i = 0; i < array.Length; i++)
 array[i] = Console.ReadLine();
 }
+void SecondArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
